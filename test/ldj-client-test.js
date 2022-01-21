4d943653dbc9780e2e7875e76e37659ca21f46ec
Sebastian <saugusth@hawk.iit.edu>
2022-01-21 02:00:17 -0600

    upgrade the test-json-service.js to be a Mocha test

--- test/ldj-client-test.js
+++ test/ldj-client-test.js
@@ -9,11 +9,12 @@ describe('LDJClient', () => {
         stream = new EventEmitter();
         client = new LDJClient(stream);
     });
-    it('should emit a message event from a single data event', done => {
+    it('should emit a message event from split data events', done => {
         client.on('message', message => {
         assert.deepEqual(message, {foo: 'bar'});
         done();
         });
-        stream.emit('data', '{"foo":"bar"}\n');
+        stream.emit('data', '{"foo":');
+        process.nextTick(() => stream.emit('data', '"bar"}\n'));
     });
 });
